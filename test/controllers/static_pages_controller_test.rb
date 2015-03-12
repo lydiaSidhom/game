250322cebc56a311285d948520d7f636a0f23382
lydiaSidhom <lydia.sidhom@gmail.com>
2015-03-12 20:41:07 +0200

    Adding about and updating titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Game for ecology"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Game for ecology"
   end
 
+  test "should get about" do
+  	get :about
+  	assert_response :success
+    assert_select "title", "About | Game for ecology"
+  end
 end
