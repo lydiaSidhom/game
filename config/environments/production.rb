6579eb354badc5a61238a0e6a93a7af8ff962cbc
lydiaSidhom <lydia.sidhom@gmail.com>
2016-02-07 18:05:47 +0200

    modifying production.rb to send emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,10 +63,27 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  
+  #config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.delivery_method = :test
+  #host = 'trafficless.herokuapp.com'
+  #config.action_mailer.default_url_options = { host: host }
+
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
   host = 'trafficless.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['lydia.sidhom'],
+    :password       => ENV['loulou123'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
