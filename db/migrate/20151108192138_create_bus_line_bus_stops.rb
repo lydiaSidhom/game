125a32260de16a8288e7210a208c519bdf529a6a
lydiaSidhom <lydia.sidhom@gmail.com>
2016-02-07 17:02:13 +0200

    remove extra migration

--- db/migrate/20151108192138_create_bus_line_bus_stops.rb
+++
@@ -1,8 +0,0 @@
-class CreateBusLineBusStops < ActiveRecord::Migration
-  def change
-    create_table :bus_line_bus_stops do |t|
-    	t.integer :bus_line_id
-     	t.integer :bus_stop_id
-    end
-  end
-end
