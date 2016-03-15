1dc1dc51e5b3944fab06fcbb6694fc49aa5b93fa
lydiaSidhom <lydia.sidhom@gmail.com>
2016-03-15 19:19:10 +0200

    trying out profile picture 2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,9 +10,9 @@ def full_title(page_title = '')
 end
 
 def avatar_url(user)
-    default_url = "/assets/images/profile.png"
+    default_url = "#{root_url}assets/images/profile.png"
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{default_url}"
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
 end
 
 end
