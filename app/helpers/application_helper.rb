6366970ace19b244de55f2ac8fe3d1f9a02711a8
lydiaSidhom <lydia.sidhom@gmail.com>
2016-03-15 19:34:22 +0200

    trying out profile picture 3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,9 +10,10 @@ def full_title(page_title = '')
 end
 
 def avatar_url(user)
-    default_url = "#{root_url}assets/images/profile.png"
+    #default_url = "#{root_url}assets/images/profile.png"
+    #default_url="http://tr3.cbsistatic.com/fly/360-fly/bundles/techrepubliccore/images/icons/standard/icon-user-default.png"
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=https://ask.libreoffice.org/m/default/media/images/nophoto.png?v=20"
 end
 
 end
